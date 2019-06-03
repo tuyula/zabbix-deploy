37477c2b0b8590a958cb3c67d1452d704a751e30
Tao Yu <taoyu666@lsrobot.vip>
2019-06-03 15:00:58 +0800

    "update dns"

--- zabbix-agent.sh
+++ zabbix-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # red 7
-# delete pre rpm
+##### delete pre rpm #####
 pre_rpm=`rpm -qa|grep zabbix-agent`
 for item in $pre_rpm
 do
@@ -11,10 +11,10 @@ do
 done
 
 yum erase -y zabbix-agent
-
 rm -rf /etc/zabbix/scripts
 rm -rf /etc/zabbix/scripts/zabbix_agentd.d
 
+##### install new rpm #####
 if cat /etc/redhat-release|grep 7\. > /dev/null;then
     rpm -ivh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm
 elif cat /etc/redhat-release|grep 6\. > /dev/null;then
@@ -23,25 +23,22 @@ else
     echo "Dont find Linux Version info:" `cat /etc/redhat-release`
 fi
 
-if grep 10\.28\.25\.25 /etc/resolv.conf > /dev/null;then
+##### add dns #####
+if ! grep 10\.28\.25\.25 /etc/resolv.conf > /dev/null;then
     echo "nameserver 10.28.25.25" >> /etc/resolv.conf
 fi
 
-if grep 10\.28\.12\.12 /etc/resolv.conf > /dev/null;then
+if ! grep 10\.28\.12\.12 /etc/resolv.conf > /dev/null;then
     echo "nameserver 10.28.12.12" >> /etc/resolv.conf
 fi
 
-
+##### start install agent #####
 yum-config-manager --enable rhel-7-server-optional-rpms
-
 yum -y install zabbix-agent, dmidecode
-
 systemctl enable zabbix-agent
 
 sed -i "s/Hostname=Zabbix server/Hostname=`dmidecode -s system-serial-number`/" /etc/zabbix/zabbix_agentd.conf
-
 sed -i 's/Server=127.0.0.1/Server=zabbix-passive.listenrobot.com/' /etc/zabbix/zabbix_agentd.conf
-
 sed -i 's/ServerActive=127.0.0.1//' /etc/zabbix/zabbix_agentd.conf
 
 cp zabbix_agentd.d /etc/zabbix/ -rf
@@ -49,11 +46,11 @@ cp scripts /etc/zabbix/ -rf
 chown zabbix:zabbix /etc/zabbix/ -R
 chown zabbix:zabbix /var/log/zabbix/ -R
 
-# install env
+##### install env #####
 yum -y install epel-release
 yum -y install python-pip
 
-# install request package
+##### install request package #####
 pip install --upgrade setuptools
 pip install --upgrade pip
 
@@ -63,7 +60,7 @@ else
     pip install simplejson
 fi
 
-# delete some file
+##### delete some file #####
 if [[ -a /etc/zabbix/zabbix_agentd.d/userparameter_mysql.conf ]];then
     rm /etc/zabbix/zabbix_agentd.d/userparameter_mysql.conf
 fi;
